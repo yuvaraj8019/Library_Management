6bddb4e616d60d9e744ad55c1e689b933a341ae7
yuvaraj8019 <yuvaraj8019@gmail.com>
2023-11-16 15:52:50 +0530

    ternary operator implemented

--- src/main/java/com/example/demo/ServiceImpl/PostServiceImplements.java
+++ src/main/java/com/example/demo/ServiceImpl/PostServiceImplements.java
@@ -89,15 +89,15 @@ public class PostServiceImplements implements PostService {
 
 	@Override
 	public PostResponse getAllPost(Integer pageNumber,Integer pageSize,String sortBy,String sortDir) {
-		
-		Sort sort=null;
-		if(sortDir.equalsIgnoreCase("asc")){
+		//ternary operator implementation
+		Sort sort=sortDir.equalsIgnoreCase("asc")?Sort.by(sortBy).ascending():Sort.by(sortBy).descending();
+		/*if(sortDir.equalsIgnoreCase("asc")){
 			sort=Sort.by(sortBy).ascending();
 		}
 		else if(sortDir.equalsIgnoreCase("desc")){
 			sort=Sort.by(sortBy).descending();
 			
-		}
+		}*/
 		
 
 		Pageable p=PageRequest.of(pageNumber, pageSize, sort);
